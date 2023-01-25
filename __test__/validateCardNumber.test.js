fd17af54c5b62829824b083a084f99dc61274d2f
radhixcode <radixxcode@gmail.com>
2023-01-25 12:20:14 +0000

    validating credid card number function

--- __test__/validateCardNumber.test.js
+++ __test__/validateCardNumber.test.js
@@ -1,10 +1,16 @@
 const cardNumber = require("../src/helper/validateCardNumber");
 
 describe("Validate credit card number test", () => {
-  test("1. Card number with less that 13", () => {
-    expect(cardNumber.validateCardNumber(111122223333)).toBeFalsy();
+  test("1. Valid credit card number", () => {
+    expect(cardNumber.validateCardNumber(377261620324999)).toBeTruthy();
   });
-  test("2. Card number with charactors", () => {
+  test("2. Credit card number with less than 13 digits", () => {
+    expect(cardNumber.validateCardNumber("111122223333")).toBeFalsy();
+  });
+  test("3. Credit card with charactors", () => {
     expect(cardNumber.validateCardNumber("111122223333aaaa")).toBeFalsy();
+  });
+  test("4. Credit card invalid number", () => {
+    expect(cardNumber.validateCardNumber(377261620324998)).toBeFalsy();
   });
 });
--- src/helper/validateCardNumber.js
+++ src/helper/validateCardNumber.js
@@ -1,8 +1,26 @@
 const luhnAlgorithm = (number) => {
-  return true;
+  // Luhn array for [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  // [0, 2, 4, 6, 8, 10, 12, 14, 16, 18] - multiplied by 2
+  // [0, 2, 4, 6, 8, 1+0, 1+2, 1+4, 1+6, 1+8] - sum double digit numbers
+  const luhnArray = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
+
+  let sum = 0;
+  let odd = false;
+
+  const numberArray = number.toString().split("").map((x) => parseInt(x));
+  const checkDigit = numberArray.pop();
+
+  for (var i = numberArray.length - 1; i >= 0; --i) {
+    if ((odd = !odd)) {
+      sum += luhnArray[numberArray[i]];
+    } else {
+      sum += numberArray[i];
+    }
+  }
+  return (sum + checkDigit) % 10 == 0;
 };
 
-exports.validateCardNumber = (cardNumber) => {
+const validateCardNumber = (cardNumber) => {
   //Check if the number contains only numeric value
   //and is of between 13 to 19 digits
   const regex = new RegExp("^[0-9]{13,19}$");
@@ -11,3 +29,5 @@ exports.validateCardNumber = (cardNumber) => {
   }
   return luhnAlgorithm(cardNumber);
 };
+
+module.exports = { validateCardNumber };
